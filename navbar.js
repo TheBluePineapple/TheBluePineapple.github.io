46dfb2eee6ff1acabba358bf1646bf0b3c5a1a30
TheBluePineapple <85315963+TheBluePineapple@users.noreply.github.com>
2022-03-20 12:36:31 -0400

    Added pages, Navbar has own files
    Added the contact, projects, and resume pages.
    Created seperated css and js files for the navigation bar.
    Made navigation bar available across all pages(see navbar.js)
    TODO: Change how I'm injecting navbar html with js, use React or web components instead
    TODO: Seperate pages and navbar specific files into their own folders

---
+++ navbar.js
@@ -0,0 +1,24 @@
+document.getElementById("navigation").innerHTML = `
+<nav id="navbar">
+    <div id="title">Title Here</div>
+    <a href="#" id="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </a>
+    <div id="nav-links">
+        <ul>
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="./projects.html">Projects</a></li>
+            <li><a href="./resume.html">Resume</a></li>
+            <li><a href="./contact.html">Contact</a></li>
+        </ul>
+    </div>
+</nav>    
+
+`;
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
