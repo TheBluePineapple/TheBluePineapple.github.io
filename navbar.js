e535f7a25841ec5983d27e17489530f8b0c9ac4a
TheBluePineapple <85315963+TheBluePineapple@users.noreply.github.com>
2022-06-25 13:12:27 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 document.getElementById("navigation").innerHTML = `
 <nav id="navbar">
-    <div id="title">Title Here</div>
+    <div id="title"><a href="https://github.com/TheBluePineapple">TheBluePineapple</a></div>
     <a href="#" id="hamburger">
         <span class="bar"></span>
         <span class="bar"></span>
